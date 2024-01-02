a4527179f98f46d28a146b83a1018616cc508851
Gabriel rodrigues <gabriel123rodrigues@hotmail.com>
2024-01-02 20:33:22 -0300

    pagina finalizar pedido

---
+++ src/pages/cadastro/index.js
@@ -0,0 +1,44 @@
+
+import React from "react";
+import logo from '../../assets/logo-white.png'
+import illustration from '../../assets/illustration.png'
+
+const Cadastro = ()=>{
+    return (<div className="container-fluid h-100 bg-primary">
+        <div className="col-12">
+            <img src={logo}></img>
+        </div>
+        <div className="row">
+        <div className="col-6 text-right my-auto">
+            <img src={illustration}></img>
+        </div>
+        <div className="col-6">
+            <div className="box col-8">
+                <h2 className="text-center">Falta pouco para fazer seu pet feliz</h2>
+                <br></br>
+                <input type="text" 
+                className="form-control form-control-lg mb-3" 
+                placeholder="Nome completo"></input>
+                <input type="text" 
+                className="form-control form-control-lg mb-3" 
+                placeholder="E-mail"></input>
+                <input type="text" 
+                className="form-control form-control-lg mb-3" 
+                placeholder="CPF"></input>
+                <input type="text" 
+                className="form-control form-control-lg mb-3" 
+                placeholder="Telefone"></input>
+                <input type="date" 
+                className="form-control form-control-lg mb-3" 
+                placeholder="Data nascimento"></input>
+                <button className="btn btn-lg btn-block btn-secondary col-12">Finalizar pedido</button>
+            </div>
+            
+        </div>
+        </div>
+      
+
+    </div>)
+}
+
+export default Cadastro;
