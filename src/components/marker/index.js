ef68ee9a5b78ccfd03f2e0bc1854aead6956bbbb
Gabriel <gabriel123rodrigues@hotmail.com>
2024-01-16 17:03:56 -0300

    correções web

---
+++ src/components/marker/index.js
@@ -0,0 +1,22 @@
+import './styles.css';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import MarkerIcon from '../../assets/marker.png';
+import MarkerIconSelected from '../../assets/marker-selected.png';
+
+const Marker = ({ petshop }) => {
+  const { petshopMapSelected } = useSelector((state) => state.shop);
+
+  return (
+    <Link to={`/petshop/${petshop._id}`}>
+      <img
+        src={
+          petshopMapSelected === petshop._id ? MarkerIconSelected : MarkerIcon
+        }
+      />
+      <img src={petshop.logo} className="img-marker" />
+    </Link>
+  );
+};
+
+export default Marker;
---
+++ src/pages/home/index.js
@@ -0,0 +1,36 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { requestPetshops } from '../../store/modules/shop/actions';
+
+import './styles.css';
+import Header from '../../components/header';
+import Petshop from '../../components/petshop';
+import Map from '../../components/map';
+
+const Home = () => {
+  const dispatch = useDispatch();
+  const { petshops } = useSelector((state) => state.shop);
+
+  useEffect(() => {
+    dispatch(requestPetshops());
+  }, []);
+
+  return (
+    <div className="h-100">
+      <Header />
+      <div className="container-fluid petshop-list-container">
+        <div className="col-12 px-4 text-center">
+          <h5>Mais próximos de você (5)</h5>
+        </div>
+        <ul className="col-12 petshop-list">
+          {petshops.map((p) => (
+            <Petshop petshop={p} />
+          ))}
+        </ul>
+      </div>
+      <Map petshops={petshops} />
+    </div>
+  );
+};
+
+export default Home;
